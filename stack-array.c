57c04c6b37c81fe52b198df521770e839ef7d7ea
Adrian Apostol <dev.adrianapostol@gmail.com>
2018-10-08 20:58:56 +0300

    stack

---
+++ stack-array.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#define MAX 5
+
+void init(int* top){
+    *top = 0;
+}
+
+
+void push(int* stack, int *top, int data) {
+    stack[(*top)++] = data;
+}
+
+int pop(int *stack, int *top){
+    return stack[--(*top)];
+}
+
+int full(int* top,const int size){
+    return *top == size ? 1: 0;
+}
+
+void display(int* stack, int* top)
+{
+    int i = 0;
+    for(i;i<*top;i++){
+        printf("%d - > %d\n", i ,stack[i]);
+    }
+}
+
+int empty(int* top){
+    return *top == 0 ? 1 : 0;
+}
+
+int main() {
+    int stack[MAX];
+    int top,elem;
+
+    init(&top);
+    empty(&top) == 1 ? printf("Empty\n") : printf("Not empty\n");
+
+    push(stack, &top, 13);
+    push(stack, &top, 10);
+    push(stack, &top, 30);
+    push(stack, &top, 130);
+    push(stack, &top, 100);
+
+    pop(stack,&top);
+    //printf("is full: %d\n",full(&top, MAX));
+    display(stack,&top);
+    full(&top, MAX) == 1 ? printf("Full\n") : printf("Not full\n");
+
+
+
+    return 0;
+}
---
+++ stack-linkedlist.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *next;
+} node;
+
+/*
+    push an element into stack
+*/
+node* push(node *head, int data){
+    node* temp = malloc(sizeof(node));
+    if(temp == NULL){
+        printf("Could not create node");
+        exit(0);
+    }
+    temp->data = data;
+    temp->next = head;
+    head = temp;
+    return head;
+}
+
+node* pop(node* head){
+    head = head->next;
+    return head;
+}
+
+void display(node* head){
+    node* curr = head;
+    if(head->next == NULL){
+        printf("Stack is empty");
+    }
+    printf("Stack: \n");
+    while(curr != NULL){
+        printf("%d\n", curr->data);
+        curr=curr->next;
+    }
+
+}
+
+void init(node* head){
+    head = NULL;
+}
+
+int main(){
+    node* head;
+    init(head);
+
+    int i;
+    for(i=0;i<=50;i++){
+        head = push(head, i);
+    }
+    /*head = push(head, 5);
+    head = push(head, 10);
+    head = push(head, 20);
+    head = push(head, 30);*/
+    display(head);
+
+    return 0;
+}

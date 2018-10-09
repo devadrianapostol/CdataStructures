e39ca6922dc8767bc50cd8c5d79abab3302405e0
Adrian Apostol <dev.adrianapostol@gmail.com>
2018-10-09 13:41:36 +0300

    C++

--- C++/stack-array.cpp
+++ C++/stack-array.cpp
@@ -3,10 +3,50 @@
 //
 #include <iostream>
 
+class Stack {
+    private:
+        int size;
+        int stack[];
+        int top;
+
+    public:
+        Stack(int Stack[],int sizeS){
+            *stack = *Stack;
+            size = sizeS;
+            top = 0;
+        };
+
+        int getSize(){
+          return size;
+        };
+
+        void displayStack(){
+            int i;
+            for(i=0;i<top;i++){
+                std::cout << stack[i] << "\n";
+            }
+        }
+
+        void push(int data){
+            stack[top] = data;
+            top += 1;
+        }
+
+
+};
+
+
+
 
 int main(){
-    const MAX = 5;
-    int stack[MAX];
-    
+    const int MAX = 5;
+    int arr[MAX] = {0,0,0,0,0};
+    Stack *stack;
+    stack = new Stack(arr, MAX);
+
+    stack->push(10);
+    stack->push(100);
+    stack->push(1000);
+    stack->displayStack();
     return 0;
 }
